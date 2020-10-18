9d57f76d6f6e888770704f87a3d5337f70676919
joan-kii <joan.sb81@gmail.com>
2020-10-18 16:50:00 +0200

    commit final. bug al añadir 'addEventListener'

--- script.js
+++ script.js
@@ -17,12 +17,14 @@ const zip = document.getElementById('zip');
 const password = document.getElementById('password');
 const passwordConf = document.getElementById('passwordConf');
 const submit = document.getElementById('submit');
+const formContainer = document.getElementById('formContainer');
+const confMessage = document.getElementById('confMessage');
 
 // Messages
 
 const emailMessage = 'Introduce un email válido';
 const countryMessage = 'Escoge un país';
-const zipMessage = 'Introduce un código postal válido';
+const zipMessage = 'Introduce un código postal válido. Máximo 5 dígitos';
 const passwordMessage = 'La contraseña debe contener 8 dígitos, con al menos un número, una mayúscula y una minúscula';
 const passwordConfMessage = 'Las contraseñas introducidas no coinciden';
 
@@ -38,19 +40,44 @@ const validation = (element, message) => {
 };
 
 const passwordCompare = (pass1, pass2) => {
-  if (pass1 === pass2) {
-    pass2.setCustomValidity('');
+  if (pass1.value === pass2.value) {
+    passwordConf.setCustomValidity('');
     return true;
   } else {
-    pass2.setCustomValidity(passwordConfMessage);
-    pass2.reportValidity();
+    passwordConf.setCustomValidity(passwordConfMessage);
+    passwordConf.reportValidity();
     return false;
   }
 };
 
-email.addEventListener('input', validation(email, emailMessage));
-country.addEventListener('input', validation(country, countryMessage));
-zip.addEventListener('input', validation(zip, zipMessage));
-password.addEventListener('input', validation(password, passwordMessage));
-passwordConf.addEventListener('input', passwordCompare(password, passwordConf));
-submit.addEventListener('click', // CONTINUAR AQUÍ. LLAMAR FUNCIÓN CREA USER)
+function createUser(email, country, zip, password) {
+  let newUser = new User(email, country, zip, password);
+  formContainer.style.display = 'none';
+  confMessage.style.display = 'block';
+  console.log(newUser)
+};
+
+email.addEventListener('input', function (event) {
+  validation(email, emailMessage);
+});
+country.addEventListener('input', function (event) {
+  validation(country, countryMessage);
+});
+zip.addEventListener('input', function (event) {
+  validation(zip, zipMessage);
+});
+password.addEventListener('input', function (event) {
+  validation(password, passwordMessage);
+});
+
+submit.addEventListener('click', function (event) {
+  event.preventDefault();
+  if (passwordCompare(password, passwordConf)) {
+    createUser(
+    email.value,
+    country.value,
+    zip.value,
+    password.value
+  )}
+});
+
